8c397974889b7f7799a246b48828ba4e3c758c4e
Luong Cong Truong <luongcongtruong@gmail.com>
2023-10-11 03:43:12 +0700

    partital completed home page

--- frontend/icons/index.js
+++ frontend/icons/index.js
@@ -1,2 +1,5 @@
+export { default as CalendarFilledIcon } from "./calendar-filled.svg";
+export { default as LocationFilledIcon } from "./location-filled.svg";
+export { default as StarFilledIcon } from "./star-filled.svg";
 export { default as TestIcon } from "./test.svg";
 export { default as UserFilledIcon } from "./user-filled.svg";

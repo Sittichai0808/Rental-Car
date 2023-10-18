429702027ae0d18b0f6bd63204b770d9536d89df
Tran Nhat Huy <nhathuy.95.tp@gmail.com>
2023-10-18 19:32:06 +0700

    createCarWCloudinary

--- backend/routes/cars.routes.js
+++ backend/routes/cars.routes.js
@@ -1,13 +1,18 @@
 import express from 'express'
-import { createCar, updateCar, getCarById, getListCars } from '../controllers/cars.controllers.js'
+import { createCar, updateCar, getCarById, getListCars, uploadImagesCar } from '../controllers/cars.controllers.js'
 import { wrapRequestHandler } from '../utils/handlers.js'
 import { accessTokenValidator, adminValidator } from '../middlewares/users.middlewares.js'
+import uploadCloud from '../utils/cloudinary.config.js'
 const carsRoutes = express.Router()
 
-carsRoutes.post('/createCar', wrapRequestHandler(createCar))
+carsRoutes.post('/createCar', uploadCloud.fields([
+    { name: 'images', maxCount: 10 },
+    { name: 'thumb', maxCount: 1 }
+]), wrapRequestHandler(createCar))
 carsRoutes.put('/updateCar/:carId', wrapRequestHandler(updateCar))
 carsRoutes.get('/:carId', wrapRequestHandler(getCarById))
-carsRoutes.get('/', accessTokenValidator, wrapRequestHandler(getListCars))
+carsRoutes.get('/', wrapRequestHandler(getListCars))
+carsRoutes.put('/uploadimage/:carId', uploadCloud.array('images', 10), wrapRequestHandler(uploadImagesCar))
 
 
 export default carsRoutes

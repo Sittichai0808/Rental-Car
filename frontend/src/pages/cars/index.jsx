dcefb94e18bffee2e5d7571f611bb69b9f8986c1
Ngo Quan Sang <Sangnq0808@gmail.com>
2023-10-26 10:02:54 +0700

    Fix deployment

--- frontend/src/pages/cars/index.jsx
+++ frontend/src/pages/cars/index.jsx
@@ -1,16 +1,13 @@
 import { CarCard } from "@/components/CarCard";
 import { SearchItem } from "@/components/SearchItem";
-import { SearchItem } from "@/components/SearchItem";
+
 import { FilterFilledIcon, SearchBrokenIcon } from "@/icons";
 import { Tag } from "antd";
 import { Button, Input } from "antd";
-// import { range } from "lodash-es";
+
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
-// import { range } from "lodash-es";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+
 import { useEffect, useState } from "react";
 export default function ListCarsPage() {
   const { isLoading, isError, data, error } = useQuery({
@@ -59,18 +56,6 @@ export default function ListCarsPage() {
                 </Button>
               )
             )}
-            {["Hãng xe", "7 chỗ", "Xe 5 sao", "Số sàn", "Số tự động"].map(
-              (item) => (
-                <Button
-                  key={item}
-
-                  // type="primary"
-                  // className="rounded-full border-none bg-slate-300 h-max py-1 px-2"
-                >
-                  {item}
-                </Button>
-              )
-            )}
           </div>
 
           <div>
@@ -91,22 +76,7 @@ export default function ListCarsPage() {
         <div className="w-1/5 flex">Sortby</div>
       </div>
 
-      <div className="w- border p-4 flex justify-center mt-2 m-auto">
-        <div className="w-4/5 flex-auto flex items-center gap-4">
-          <SearchItem name="Hãng Xe" />
-          <SearchItem name="Số chỗ" />
-          <SearchItem name="Đánh giá" />
-          <SearchItem name="Truyền động" />
-        </div>
-        <div className="w-1/5 flex">Sortby</div>
-      </div>
-
       <div className="grid grid-cols-4 gap-x-4 gap-y-6 mt-10">
-        {isLoading ? (
-          <span>Loading...</span>
-        ) : (
-          data.map((idx, index) => <CarCard key={index} dataCar={idx} />)
-        )}
         {isLoading ? (
           <span>Loading...</span>
         ) : (

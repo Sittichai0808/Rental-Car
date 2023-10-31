d9a57e0ccf161252dea8cef83de59692f7ed8e02
Tran Nhat Huy <huytnde150173@fpt.edu.vn>
2023-10-31 17:45:50 +0700

    update api, add getBrands

--- backend/services/brands.services.js
+++ backend/services/brands.services.js
@@ -10,6 +10,16 @@ class BrandsService {
         } catch (error) {
         }
     }
+
+    async getBrands() {
+        try {
+            const getBrands = Brands.find()
+            return getBrands
+        } catch (error) {
+            throw error
+        }
+
+    }
 }
 const brandsService = new BrandsService()
 export default brandsService

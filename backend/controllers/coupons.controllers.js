9b1eb47a708e3f73e4466c6831ed174f9af5e168
Tran Nhat Huy <huytnde150173@fpt.edu.vn>
2023-11-07 00:49:05 +0700

    coupons api

---
+++ backend/controllers/coupons.controllers.js
@@ -0,0 +1,54 @@
+import couponsService from '../services/coupons.services.js'
+import { HTTP_STATUS } from '../constants/httpStatus.js'
+
+export const createCoupons = async (req, res, next) => {
+    try {
+        const expiry = Date.now() + req.body.expiry * 24 * 60 * 60 * 1000;
+        const result = await couponsService.createCoupons(req.body, expiry);
+
+        return res.status(HTTP_STATUS.CREATED).json({
+            message: 'Created coupon successfully',
+            result
+        });
+    } catch (error) {
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            message: "Something went wrong",
+            error: error.message
+        })
+    }
+}
+
+export const getCoupons = async (req, res, next) => {
+    try {
+        const result = await couponsService.getCoupons()
+        return res.status(HTTP_STATUS.OK).json({
+            message: 'Get coupons successfully',
+            result
+        })
+    } catch (error) {
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            message: 'Something went wrong',
+            error: error.message
+        })
+    }
+}
+
+export const updateCoupons = async (req, res, next) => {
+    try {
+        const { cid } = req.params
+        const result = await couponsService.updateCoupons(cid, req.body)
+        if (!result) {
+            return res.status(HTTP_STATUS.NOT_FOUND).json({ error: 'Coupon not found' })
+        } else {
+            return res.status(HTTP_STATUS.OK).json({
+                message: 'Coupon Updated',
+                result
+            })
+        }
+    } catch (error) {
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            message: 'Something went wrong',
+            error: error.message
+        })
+    }
+}

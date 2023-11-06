794b31aef98ba0093a748bd2f84aaa089a86ca9d
Tran Nhat Huy <huytnde150173@fpt.edu.vn>
2023-11-06 22:26:34 +0700

    uploadImages

--- backend/routes/cars.routes.js
+++ backend/routes/cars.routes.js
@@ -8,23 +8,28 @@ import {
   ratings
 } from '../controllers/cars.controllers.js'
 import { wrapRequestHandler } from '../utils/handlers.js'
-import { accessTokenValidator, adminValidator } from '../middlewares/users.middlewares.js'
+import { accessTokenValidator, adminValidator, staffValidator } from '../middlewares/users.middlewares.js'
 import uploadCloud from '../utils/cloudinary.config.js'
 const carsRoutes = express.Router()
 
 carsRoutes.post(
   '/createCar',
-  accessTokenValidator,
-  uploadCloud.fields([
-    { name: 'images', maxCount: 10 },
-    { name: 'thumb', maxCount: 1 }
-  ]),
+  [accessTokenValidator, adminValidator, staffValidator],
   wrapRequestHandler(createCar)
 )
 carsRoutes.put('/updateCar/:carId', accessTokenValidator, adminValidator, wrapRequestHandler(updateCar))
 carsRoutes.get('/:carId', wrapRequestHandler(getCarById))
 carsRoutes.get('/', wrapRequestHandler(getListCars))
-carsRoutes.put('/uploadimage/:carId', uploadCloud.array('images', 10), wrapRequestHandler(uploadImagesCar))
+carsRoutes.post('/uploadimage/:carId', uploadCloud.fields([
+  { name: 'images', maxCount: 10 },
+  { name: 'thumb', maxCount: 1 }
+]), wrapRequestHandler(uploadImagesCar))
+
+carsRoutes.put('/uploadimage/:carId', uploadCloud.fields([
+  { name: 'images', maxCount: 10 },
+  { name: 'thumb', maxCount: 1 }
+]), wrapRequestHandler(uploadImagesCar))
+
 carsRoutes.post('/:carId/rating', accessTokenValidator, wrapRequestHandler(ratings))
 
 export default carsRoutes

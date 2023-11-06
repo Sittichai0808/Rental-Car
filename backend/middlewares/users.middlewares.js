eee7c04eb8eb66ef3e0a28beb0b4f1220e69c054
Tran Nhat Huy <nhathuy.95.tp@gmail.com>
2023-11-06 22:35:38 +0700

    update middleware

--- backend/middlewares/users.middlewares.js
+++ backend/middlewares/users.middlewares.js
@@ -214,105 +214,6 @@ export const accessTokenValidator = validate(
   )
 )
 
-export const staffValidator = validate(
-  checkSchema(
-    {
-      authorization: {
-        trim: true,
-        custom: {
-          options: async (value, { req }) => {
-            if (!value) {
-              throw new ErrorWithStatus({
-                message: USER_MESSAGES.ACCESS_TOKEN_IS_REQUESTED,
-                status: HTTP_STATUS.UNAUTHORIZED
-              })
-            }
-            const access_token = (value || '').split(' ')[1]
-
-            if (!access_token) {
-              throw new ErrorWithStatus({
-                message: USER_MESSAGES.ACCESS_TOKEN_IS_REQUESTED,
-                status: HTTP_STATUS.UNAUTHORIZED
-              })
-            }
-
-            try {
-              const decoded_authorization = await verifyToken({
-                token: token,
-                secretOrPublickey: process.env.JWT_SECRET_ACCESS_TOKEN
-              })
-              const { role } = decoded_authorization
-              if (role === 'staff') {
-                req.decoded_authorization = decoded_authorization
-                next()
-              } else {
-                next(new ErrorWithStatus('You not staff', HTTP_STATUS.UNAUTHORIZED).errorHandler())
-              }
-            } catch (error) {
-              if (typeof error === JsonWebTokenError) {
-                const error = new ErrorWithStatus(error?.message, HTTP_STATUS.UNAUTHORIZED)
-                next(error.errorHandler())
-              }
-            }
-
-            return true
-          }
-        }
-      }
-    },
-    ['headers']
-  )
-)
-export const adminValidator = validate(
-  checkSchema(
-    {
-      authorization: {
-        trim: true,
-        custom: {
-          options: async (value, { req }) => {
-            if (!value) {
-              throw new ErrorWithStatus({
-                message: USER_MESSAGES.ACCESS_TOKEN_IS_REQUESTED,
-                status: HTTP_STATUS.UNAUTHORIZED
-              })
-            }
-            const access_token = (value || '').split(' ')[1]
-
-            if (!access_token) {
-              throw new ErrorWithStatus({
-                message: USER_MESSAGES.ACCESS_TOKEN_IS_REQUESTED,
-                status: HTTP_STATUS.UNAUTHORIZED
-              })
-            }
-
-            try {
-              const decoded_authorization = await verifyToken({
-                token: token,
-                secretOrPublickey: process.env.JWT_SECRET_ACCESS_TOKEN
-              })
-              const { role } = decoded_authorization
-              if (role === 'admin') {
-                req.decoded_authorization = decoded_authorization
-                next()
-              } else {
-                next(new ErrorWithStatus('You not admin', HTTP_STATUS.UNAUTHORIZED).errorHandler())
-              }
-            } catch (error) {
-              if (typeof error === JsonWebTokenError) {
-                const error = new ErrorWithStatus(error?.message, HTTP_STATUS.UNAUTHORIZED)
-                next(error.errorHandler())
-              }
-            }
-
-            return true
-          }
-        }
-      }
-    },
-    ['headers']
-  )
-)
-
 // export const accessTokenValidator = async (req, res, next) => {
 //   const token = req.access_token
 
@@ -424,4 +325,12 @@ export const adminValidator = validate(
                 next(error.errorHandler())
               }
             }
+
+            return true
           }
+        }
+      }
+    },
+    ['headers']
+  )
+)

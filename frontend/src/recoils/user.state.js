dfe10c5b299f6e06dddb8615c0148619d1ea18bb
Tran Nhat Huy <nhathuy.95.tp@gmail.com>
2023-11-17 00:16:30 +0700

    fix merge Thinhdn

--- frontend/src/recoils/user.state.js
+++ frontend/src/recoils/user.state.js
@@ -1,10 +1,24 @@
 import { atom, useRecoilState, useRecoilValue } from "recoil";
-
-const getProfile = (key) => {
+import { apiClient } from "../apis/client";
+const getProfile = async () => {
   try {
     if (typeof window !== "undefined") {
-      const value = window.localStorage.getItem(key);
-      return value ? JSON.parse(value) : null;
+      const value = window.localStorage.getItem("access_token");
+
+      if (value !== null) {
+        const { data } = await apiClient.request({
+          method: "GET",
+          url: "/users/get-user",
+          headers: {
+            Authorization: `Bearer ${JSON.parse(value)}`,
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        });
+        console.log(data);
+
+        return data;
+      }
     }
     return null;
   } catch (error) {
@@ -14,7 +28,8 @@ const getProfile = (key) => {
 
 export const userAtom = atom({
   key: "user",
-  default: getProfile("profile"),
+  default: getProfile(),
 });
+
 export const useUserState = () => useRecoilState(userAtom);
 export const useUserValue = () => useRecoilValue(userAtom);

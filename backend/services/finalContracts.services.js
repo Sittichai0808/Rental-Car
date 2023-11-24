929b738f042f0a85738695b22facc3da6470d711
Ngo Quan Sang <Sangnq0808@gmail.com>
2023-11-24 21:39:39 +0700

    Done api final contract

--- backend/services/finalContracts.services.js
+++ backend/services/finalContracts.services.js
@@ -1,26 +1,40 @@
 import Bookings from '../models/booking.model.js'
 import Contracts from '../models/contracts.model.js'
 import FinalContracts from '../models/finalContracts.model.js'
+import BookedTimeSlots from '../models/bookedTimeSlots.model.js'
 import moment from 'moment-timezone'
 class FinalContractsService {
   async createFinalContract(contractId, payload) {
     try {
       const { file, cost_settlement, timeFinish, note } = payload
       // Parse the input date using the 'DD-MM-YYYY' format and set the timezone to Asia/Ho_Chi_Minh (ICT)
-      const formattedTimeFinish = moment.tz(timeFinish, 'DD-MM-YYYY', 'Asia/Ho_Chi_Minh').toDate()
+      const formattedTimeFinish = moment.tz(timeFinish, 'YYYY-MM-DD', 'Asia/Ho_Chi_Minh').toDate()
       const newFinalContract = new FinalContracts({
         contractId: contractId,
-        file: file,
-        cost_settlement: cost_settlement,
         timeFinish: formattedTimeFinish,
-        note: note
+        ...payload
       })
 
-      await newFinalContract.save()
+      const finalContractResult = await newFinalContract.save()
+      const getFinalContract = await FinalContracts.find({ _id: finalContractResult._id }).populate({
+        path: 'contractId',
+        populate: {
+          path: 'bookingId',
+          model: 'Bookings'
+        }
+      })
+
+      await BookedTimeSlots.findOneAndUpdate(
+        { bookingId: getFinalContract[0].contractId.bookingId._id },
+        { $set: { to: formattedTimeFinish } },
+        { new: true }
+      )
 
       await Contracts.findByIdAndUpdate(contractId, { $set: { status: 'Đã tất toán' } }, { new: true })
 
-      return newFinalContract
+      // await Contracts.findByIdAndUpdate(contractId, { $set: { status: 'Đã tất toán' } }, { new: true })
+
+      return [finalContractResult, getFinalContract]
     } catch (error) {
       throw new Error(error)
     }

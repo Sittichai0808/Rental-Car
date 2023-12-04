905a90ae2bb5967a8969d86bbfa0f2207ca6c8bf
Tran Nhat Huy <nhathuy.95.tp@gmail.com>
2023-12-05 04:10:02 +0700

    api admin/staff dashboard

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import { adminAndStaffValidator, adminValidator } from '../middlewares/users.middlewares.js'
+import { adminAndStaffValidator, adminValidator, staffValidator } from '../middlewares/users.middlewares.js'
 import { wrapRequestHandler } from '../utils/handlers.js'
-import { getUsers, getStaffs, getDetailUser, createStaff, updateStatusUser } from '../controllers/admins.controllers.js'
+import { getUsers, getStaffs, getDetailUser, createStaff, updateStatusUser, totalAdminDashboard, totalStaffDashboard, getTotalRevenue } from '../controllers/admins.controllers.js'
 const adminRoutes = express.Router()
 
 adminRoutes.get('/list-users', adminAndStaffValidator, wrapRequestHandler(getUsers))
@@ -9,5 +9,7 @@ adminRoutes.get('/staffs', adminValidator, wrapRequestHandler(getStaffs))
 adminRoutes.post('/create-staffs', adminValidator, wrapRequestHandler(createStaff))
 adminRoutes.put('/update-status/:userId', adminAndStaffValidator, wrapRequestHandler(updateStatusUser))
 adminRoutes.get('/get-user/:userId', adminAndStaffValidator, wrapRequestHandler(getDetailUser))
-
+adminRoutes.get('/admin-dashboard', adminValidator, wrapRequestHandler(totalAdminDashboard))
+adminRoutes.get('/staff-dashboard', staffValidator, wrapRequestHandler(totalStaffDashboard))
+adminRoutes.get('/total-revenue-by-month', adminAndStaffValidator, wrapRequestHandler(getTotalRevenue))
 export default adminRoutes
--- backend/services/admin.services.js
+++ backend/services/admin.services.js
@@ -1,4 +1,9 @@
 import User from "../models/user.model.js"
+import Cars from "../models/cars.model.js"
+import Bookings from "../models/booking.model.js"
+import Contracts from "../models/contracts.model.js"
+import FinalContracts from "../models/finalContracts.model.js"
+import mongoose from "mongoose"
 import { hashPassword } from '../utils/crypto.js'
 
 class AdminServices {
@@ -51,6 +56,127 @@ class AdminServices {
             throw error
         }
     }
+
+    async totalAdminDashboard() {
+        try {
+            const currentDate = new Date();
+            const currentMonth = currentDate.getMonth() + 1
+            const totalUsers = await User.countDocuments()
+            const totalCars = await Cars.countDocuments()
+            const bookingByMonth = await Bookings.aggregate([
+                {
+                    $match: {
+                        $expr: {
+                            $and: [
+                                { $eq: [{ $month: '$timeBookingStart' }, currentMonth] },
+                                { $eq: [{ $month: '$timeBookingEnd' }, currentMonth] },
+                            ]
+                        }
+                    }
+                },
+                {
+                    $count: 'totalResults'
+                }
+            ]);
+            const totalBookingByMonth = bookingByMonth.length > 0 ? bookingByMonth[0].totalResults : 0
+            const revenue = await FinalContracts.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalCost: { $sum: "$cost_settlement" }
+                    }
+                }
+            ])
+            const totalRevenue = revenue.length > 0 ? revenue[0].totalCost : 0
+            return { totalUsers, totalCars, totalBookingByMonth, totalRevenue }
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async totalStaffDashboard(staffId) {
+        try {
+            const ObjectId = mongoose.Types.ObjectId
+            const totalCarsCreated = await Cars.find({ user: staffId }).count()
+            const totalContractCreated = await Contracts.find({ createBy: staffId }).count()
+            const bookingByMonth = await Bookings.aggregate([
+                {
+                    $match: {
+                        $expr: {
+                            $and: [
+                                { $eq: [{ $month: '$timeBookingStart' }, currentMonth] },
+                                { $eq: [{ $month: '$timeBookingEnd' }, currentMonth] },
+                            ]
+                        }
+                    }
+                },
+                {
+                    $count: 'totalResults'
+                }
+            ]);
+            const totalBookingByMonth = bookingByMonth.length > 0 ? bookingByMonth[0].totalResults : 0
+            const finalContracts = await FinalContracts.aggregate([
+                {
+                    $lookup: {
+                        from: 'contracts',
+                        localField: 'contractId',
+                        foreignField: '_id',
+                        as: 'contractInfo'
+                    }
+                },
+                {
+                    $match: {
+                        'contractInfo.createBy': new ObjectId(staffId)
+                    }
+                },
+                {
+                    $count: 'totalResults'
+                }
+            ]);
+            const totalFinalContracts = finalContracts.length > 0 ? finalContracts[0].totalResults : 0;
+
+            return { totalCarsCreated, totalBookingByMonth, totalContractCreated, totalFinalContracts }
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+    async getTotalRevenue() {
+        try {
+            const finalContracts = await FinalContracts.find();
+            const currentYear = new Date().getFullYear();
+
+            const revenueByMonth = finalContracts.reduce((result, contract) => {
+                const finishDate = new Date(contract.timeFinish);
+
+                const month = finishDate.getMonth() + 1;
+                const year = finishDate.getFullYear();
+
+                // Only include months from the current year
+                if (year === currentYear) {
+                    const key = `${year}-${month}`;
+
+                    if (!result[key]) {
+                        result[key] = {
+                            month: `${month}`,
+                            totalRevenue: 0
+                        };
+                    }
+
+                    result[key].totalRevenue += contract.cost_settlement;
+                }
+
+                return result;
+            }, {});
+
+            const revenueArray = Object.values(revenueByMonth);
+            return revenueArray;
+        } catch (error) {
+            console.error('Error in getTotalRevenue:', error);
+            throw new Error('Error fetching total revenue');
+        }
+    }
+
 }
+
 const adminServices = new AdminServices()
 export default adminServices
